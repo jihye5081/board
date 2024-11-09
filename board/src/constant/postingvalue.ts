9ce9357b352468b07366e7377eecf8ae1a10fdf0
jihye5081 <ekftn90@gmail.com>
2024-11-09 22:36:03 +0900

    feat: 게시판 리스트 UI 작성

---
+++ board/src/constant/postingvalue.ts
@@ -0,0 +1,45 @@
+interface postingValueI {
+    author: string
+    title: string
+    date: Date
+    content: string
+}
+
+export const postingValue : postingValueI[]= [
+    {
+        author:"yunmi",
+        title:"yunmi's journey",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },{
+        author:"yunmi",
+        title:"yunmi's journey2",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },{
+        author:"yunmi",
+        title:"yunmi's journey3",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },{
+        author:"yunmi",
+        title:"yunmi's journey4",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },{
+        author:"yunmi",
+        title:"yunmi's journey5",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },{
+        author:"yunmi",
+        title:"yunmi's journey6",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },{
+        author:"yunmi",
+        title:"yunmi's journey7",
+        date:new Date(),
+        content:"yunmi's journey test contents"
+    },
+]
--- board/src/page/BoardList.tsx
+++ board/src/page/BoardList.tsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import styled from 'styled-components'
+import { postingValue } from '../constant/postingvalue'
 
 export default function BoardList() {
   return (
-    <div>
-      
-    </div>
+    <Wrapper>
+        <Section>
+            {
+                postingValue.map((el,idx)=>(
+                    <ListArticle>
+                        <ListIndex>{idx+1}.</ListIndex>
+                        <ListElement>{el.title}</ListElement>   
+                    </ListArticle>
+                ))
+            }
+        </Section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    border: 4px solid black;
+    width: 800px;
+    height: 1000px;
+    font-size: 32px;
+`
+
+const ListArticle= styled.article`
+    width: 100%;
+    display: flex;
+    background-color: lightgoldenrodyellow;
+`
+
+const ListIndex = styled.div`
+    display: flex;
+    width: 10%;
+    height: fit-content;
+    padding: 8px 0;
+`
+const ListElement = styled.div`
+    display: flex;
+    width: 90%;
+    height: fit-content;
+    padding: 8px 0;
+`
