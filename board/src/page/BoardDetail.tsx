d1c7315c9d1e7864eb295a61a19e35863fd8db5f
jihye5081 <ekftn90@gmail.com>
2024-11-14 22:41:50 +0900

    feat: 게시판 상세보기 기본 UI 작성

--- board/src/page/BoardDetail.tsx
+++ board/src/page/BoardDetail.tsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import styled from 'styled-components'
+import { postingValue } from '../constant/postingvalue'
 
 export default function BoardDetail() {
   return (
-    <div>
-    </div>
+    <Wrapper>
+      <Section>
+        <ListTitle>title</ListTitle>
+        <ListWrapper>
+          <ListWriter>writername</ListWriter>
+          <ListDate>date</ListDate>
+        </ListWrapper>
+        <ListContent>
+        </ListContent>
+      </Section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 800px;
+  height: 1000px;
+  font-size: 32px;
+  border: 4px solid black;
+`
+
+const ListTitle = styled.header`
+  display: flex;
+  flex-direction: column;
+  font-size: 52px;
+  font-weight: bold;
+  text-align: center;
+`
+
+const ListWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ListWriter = styled.div`
+  display: flex;
+  font-size: 32px;
+`
+
+const ListDate = styled.div`
+  display: flex;
+  font-size: 32px;
+`
+
+const ListContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 32px;
+`
